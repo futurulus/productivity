01b284da010450f3f9b00f0acbb285a53ee57fed
Will Monroe <wmonroe4@stanford.edu>
2016-04-22 01:13:37 -0700

    Add initial implementation (fast for testing)

---
+++ indicator.py
@@ -0,0 +1,91 @@
+import datetime
+from os.path import abspath
+import sys
+
+import gtk
+import appindicator as ind
+
+import productivity as prod
+
+ICONS = {
+    prod.WORKING: abspath('working.svg'),
+    prod.PLAYING: abspath('playing.svg'),
+    prod.CRUNCH_MODE: abspath('crunch-mode.svg'),
+    prod.RED_ALERT: abspath('red-alert.svg'),
+}
+
+
+class ProductivityIndicator(object):
+    '''
+    Implementation based on AppIndicator tutorial from ConjureCode:
+    http://conjurecode.com/create-indicator-applet-for-ubuntu-unity-with-python/
+    '''
+    def __init__(self):
+        self.ind = ind.Indicator('productivity', 'media-seek-forward-symbolic',
+                                 ind.CATEGORY_APPLICATION_STATUS)
+        self.ind.set_status(ind.STATUS_ACTIVE)
+        self.menu = self.build_menu()
+        self.ind.set_menu(self.menu)
+
+        self.prod = prod.Productivity()
+
+    def build_menu(self):
+        menu = gtk.Menu()
+        self.clockin_item = self.add_item(menu, prod.WORKING.label, self.clockin)
+        self.clockout_item = self.add_item(menu, prod.PLAYING.label, self.clockout)
+        self.add_item(menu, 'Quit', self.quit)
+        return menu
+
+    def add_item(self, menu, label, func):
+        item = gtk.MenuItem(label)
+        item.connect('activate', func)
+        item.show()
+        menu.append(item)
+        return item
+
+    def main(self):
+        gtk.timeout_add(1000, self.update)
+        gtk.main()
+
+    def clockin(self, _):
+        self.prod.clockin()
+        self.update()
+
+    def clockout(self, _):
+        self.prod.clockout()
+        self.update()
+
+    def quit(self, _):
+        self.prod.quit()
+        sys.exit(0)
+
+    def update(self):
+        self.ind.set_label(format_timedelta(self.prod.uptime()))
+        self.ind.set_icon(ICONS[self.prod.status()])
+
+        self.clockin_item.set_label(prod.CRUNCH_MODE.label
+                                    if self.prod.late else
+                                    prod.WORKING.label)
+        self.clockout_item.set_label(prod.RED_ALERT.label
+                                     if self.prod.late else
+                                     prod.PLAYING.label)
+
+        self.clockin_item.set_sensitive(not self.prod.working)
+        self.clockout_item.set_sensitive(self.prod.working)
+        return True
+
+
+def format_timedelta(td):
+    if td < datetime.timedelta(0):
+        td_str = '-' + str(-td)
+    else:
+        td_str = str(td)
+    point = td_str.find('.')
+    if point == -1:
+        return td_str
+    else:
+        return td_str[:point]
+
+
+if __name__ == '__main__':
+    ProductivityIndicator().main()
---
+++ productivity.py
@@ -0,0 +1,115 @@
+import os
+import datetime
+from collections import namedtuple
+
+
+Status = namedtuple('Status', ['id', 'label'])
+
+WORKING = Status('WORKING', 'Working')
+PLAYING = Status('PLAYING', 'Playing')
+CRUNCH_MODE = Status('CRUNCH_MODE', 'Crunch mode')
+RED_ALERT = Status('RED_ALERT', 'Red Alert!')
+
+LOG_DIR = os.path.expanduser('~/.productivity')
+
+
+def mkdirp(dirname):
+    '''
+    Create a directory at the path given by `dirname`, if it doesn't
+    already exist.
+
+    http://stackoverflow.com/a/14364249/4481448
+    '''
+    try:
+        os.makedirs(dirname)
+    except OSError:
+        if not os.path.isdir(dirname):
+            raise
+
+
+class Productivity(object):
+    def __init__(self):
+        self.uptime_ = datetime.timedelta(0)
+        self.status_ = PLAYING
+        self.working = False
+        mkdirp(LOG_DIR)
+        t, u = datetime.datetime.now(), datetime.datetime.utcnow()
+        self.log_event(t, u, 'start')
+        self.last_clockin = u
+        self.update(t, u)
+    
+    def uptime(self):
+        t, u = datetime.datetime.now(), datetime.datetime.utcnow()
+        self.update(t, u)
+        return self.uptime_
+
+    def status(self):
+        t, u = datetime.datetime.now(), datetime.datetime.utcnow()
+        self.update(t, u)
+        return self.status_
+
+    def clockin(self):
+        t, u = datetime.datetime.now(), datetime.datetime.utcnow()
+        self.log_event(t, u, 'clockin')
+        self.last_clockin = u
+        self.working = True
+        self.update(t, u)
+
+    def clockout(self):
+        t, u = datetime.datetime.now(), datetime.datetime.utcnow()
+        self.log_event(t, u, 'clockout')
+        self.working = False
+        self.update(t, u)
+
+    def quit(self):
+        t, u = datetime.datetime.now(), datetime.datetime.utcnow()
+        self.log_event(t, u, 'quit')
+
+    def update(self, t, u):
+        if self.status_ == WORKING:
+            self.uptime_ += u - self.last_update
+        elif self.status_ == RED_ALERT:
+            self.uptime_ -= u - self.last_update
+
+        if self.working and u - self.last_clockin >= self.reset_interval():
+            self.working = False
+
+        old_status = self.status_
+        self.late = self.is_late(t)
+        if self.late:
+            self.status_ = CRUNCH_MODE if self.working else RED_ALERT
+        else:
+            self.status_ = WORKING if self.working else PLAYING
+
+        if self.status_ != old_status:
+            self.log_event(t, u, 'update')
+
+        self.last_update = u
+
+    def log_event(self, t, u, name):
+        t_format = format_datetime(t)
+        u_format = format_datetime(u)
+        up_format = format_timedelta(self.uptime_)
+        log_line('\t'.join((t_format, u_format, up_format,
+                            self.status_.id, name)))
+
+    def reset_interval(self):
+        return datetime.timedelta(seconds=15)
+
+    def is_late(self, t):
+        return t.second % 10 >= 6
+
+
+def format_datetime(t):
+    return ':'.join(str(e) for e in (t.year, t.month, t.day,
+                                     t.hour, t.minute, t.second, t.microsecond))
+
+
+def format_timedelta(t):
+    return ':'.join(str(e) for e in (t.days, t.seconds, t.microseconds))
+
+
+def log_line(line):
+    with open(os.path.join(LOG_DIR, 'events.log'), 'a') as outfile:
+        outfile.write(line + '\n')
+
